b2e0f2bfb9254555ad62e4791042ec88e2cf2092
Sercan Leylek <leylek.sercan@gmail.com>
2021-05-24 12:05:04 +0200

    Go to homepage on Logo Click

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,12 +1,17 @@
 import { Link } from "@fremtind/jkl-core";
+import history from "../../Utilities/History";
 import "./Nav.scss";
 
 export const Nav = () => {
+    const goToHomePage = () => {
+        history.push('/');
+    };
+
     return (
         <nav className="heading-nav">
-            <p className="heading-nav__brand">Runer</p>
+            <p onClick={goToHomePage} className="heading-nav__brand">Runer</p>
             <p className="heading-nav__story">En lesbarhetstest laget av Fremtind</p>
-            <Link href="https://jokul.fremtind.no">Gå til Designsystemet</Link>
+            <Link href="https://jokul.fremtind.no">Gå til designsystemet →</Link>
         </nav>
     );
 };
