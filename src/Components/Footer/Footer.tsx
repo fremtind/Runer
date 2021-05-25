9c29393f7580b0b556c314c72506da9196e02667
Jo Emil Holen <jo.emil.holen@fremtind.no>
2021-05-25 11:23:42 +0200

    center item if it's only one item

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Children } from "react";
 import "./Footer.scss";
 
 export const Footer: React.FC = ({ children }) => {
-    return <footer className="footer">{children}</footer>;
+    return <footer className={`footer ${Children.count(children) === 1 ? "footer--single" : ""}`}>{children}</footer>;
 };
